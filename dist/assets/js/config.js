36497669c95df29b1e48a546c75d82bcf5c72d01
Pedro Guimarães <pedrogaf55@gmail.com>
2025-11-01 17:15:55 -0300

    feat(config): adicionar configuração do endpoint da API do leaderboard

--- dist/assets/js/config.js
+++ dist/assets/js/config.js
@@ -1,1 +1,7 @@
-window.LEADERBOARD_API=window.LEADERBOARD_API||"";
+/**
+ * Configuração opcional do endpoint de leaderboard (API externa).
+ *
+ * Caso você hospede o site estático (HTML/CSS/JS) em um lugar
+ * e a API do placar (Top 10) em outro (ex.: Vercel ou Apps Script), defina abaixo:
+ */
+window.LEADERBOARD_API = window.LEADERBOARD_API || 'https://script.google.com/macros/s/AKfycbwNFOo7vdeMmjn5IimQjghcJb0RyhygPJv4DfGNf1ypiX0AV4uPgkINB8pOTIylTku6WQ/exec';
